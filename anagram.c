c17c1256ac01e4e2b1d0aed8ec99677f8fd99c4a
M. Brent Harp <brharp@uoguelph.ca>
2020-06-10 21:21:41 -0400

    Open file

--- anagram.c
+++ anagram.c
@@ -5,6 +5,10 @@
 
 #define MAX_WORD 500
 
+#ifndef DICT
+#define DICT "/usr/share/dict/american-english"
+#endif
+
 static int
 cmpstringp(const void *p1, const void *p2)
 {
@@ -13,6 +17,7 @@ cmpstringp(const void *p1, const void *p2)
 
 int main(int argc, char *argv[])
 {
+	FILE *dict;
 	char word[MAX_WORD];
 	char needle[MAX_WORD];
 	char scramble[MAX_WORD];
@@ -23,11 +28,17 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	dict = fopen(DICT, "r");
+	if (dict == NULL) {
+		fprintf(stderr, "File not found: %s\n", DICT);
+		exit(EXIT_FAILURE);
+	}
+
 	strcpy(needle, argv[1]);
 	n = strlen(needle);
 	qsort(needle, n, 1, cmpstringp);
 
-	while (fgets(word, MAX_WORD-1, stdin) != NULL)
+	while (fgets(word, MAX_WORD-1, dict) != NULL)
 	{
 		n = strlen(word);
 
